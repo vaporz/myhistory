1c56fba26c59f3486ef8054c8ea801d6662a8b8d
vaporz <zzxx513@gmail.com>
2013-03-29 19:20:23 +0800

    redeploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+SERVER=$1
+if [ -z "${SERVER}" ]; then
+        echo "Usage:deploy.sh [ip]";
+    exit;
+fi
+
 mvn -U clean package
-echo 'rm -rf /opt/www/myhistory'
-rm -rf /opt/www/myhistory 
-echo 'cp -r ./target/myhistory-0.0.1-SNAPSHOT /opt/www/myhistory'
-cp -r ./target/myhistory-0.0.1-SNAPSHOT /opt/www/myhistory
-sh /opt/soft/resin/bin/resin.sh -server myhistory kill
-sh /opt/soft/resin/bin/resin.sh -server myhistory start
+echo 'ssh zhangxiao@$SERVER rm -rf /opt/zhangxiao/myhistory.tmp'
+ssh zhangxiao@$SERVER "rm -rf /opt/zhangxiao/myhistory.tmp"
+echo 'scp -rp ./target/myhistory-0.0.1-SNAPSHOT zhangxiao@$SERVER:/opt/zhangxiao/myhistory.tmp'
+scp -rp ./target/myhistory-0.0.1-SNAPSHOT zhangxiao@$SERVER:/opt/zhangxiao/myhistory.tmp
+echo 'ssh zhangxiao@$SERVER cd /opt/zhangxiao; ln -s /opt/zhangxiao/myhistory.tmp myhistory;sh /opt/soft/resin/bin/resin.sh -server myhistory kill;sh /opt/soft/resin/bin/resin.sh -server myhistory start'
+ssh zhangxiao@$SERVER "cd /opt/zhangxiao; ln -s /opt/zhangxiao/myhistory.tmp myhistory;sh /opt/soft/resin/bin/resin.sh -server myhistory kill;sh /opt/soft/resin/bin/resin.sh -server myhistory start"
