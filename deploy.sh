4228d3efd9873c3997dadca816bb5c62974fc419
vaporz <zzxx513@gmail.com>
2013-04-01 00:57:23 +0800

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,10 +5,14 @@ if [ -z "${SERVER}" ]; then
     exit;
 fi
 
+DATE=`date +%s`
+
 mvn -U clean package
-echo 'ssh zhangxiao@$SERVER rm -rf /opt/zhangxiao/myhistory.tmp'
-ssh zhangxiao@$SERVER "rm -rf /opt/zhangxiao/myhistory.tmp"
-echo 'scp -rp ./target/myhistory-0.0.1-SNAPSHOT zhangxiao@$SERVER:/opt/zhangxiao/myhistory.tmp'
-scp -rp ./target/myhistory-0.0.1-SNAPSHOT zhangxiao@$SERVER:/opt/zhangxiao/myhistory.tmp
-echo 'ssh zhangxiao@$SERVER cd /opt/zhangxiao; ln -s /opt/zhangxiao/myhistory.tmp myhistory;sh /opt/soft/resin/bin/resin.sh -server myhistory kill;sh /opt/soft/resin/bin/resin.sh -server myhistory start'
-ssh zhangxiao@$SERVER "cd /opt/zhangxiao; ln -s /opt/zhangxiao/myhistory.tmp myhistory;sh /opt/soft/resin/bin/resin.sh -server myhistory kill;sh /opt/soft/resin/bin/resin.sh -server myhistory start"
+cd target
+mv myhistory-0.0.1-SNAPSHOT myhistory.$DATE
+tar czvf myhistory.tar myhistory.$DATE
+cd ..
+echo "scp -rp ./target/myhistory.tar zhangxiao@$SERVER:/opt/zhangxiao/www/"
+      scp -rp -C -l 1024 ./target/myhistory.tar zhangxiao@$SERVER:/opt/zhangxiao/www/
+echo "ssh zhangxiao@$SERVER "cd /opt/zhangxiao/www; tar xzvf myhistory.tar;ln -s myhistory.$DATE myhistory;sh /opt/soft/resin/bin/resin.sh -server myhistory kill;sh /opt/soft/resin/bin/resin.sh -server myhistory start"
+     ssh zhangxiao@$SERVER "cd /opt/zhangxiao/www; tar xzvf /opt/zhangxiao/www/myhistory.tar;rm -rf myhistory; ln -s myhistory.$DATE myhistory;sh /opt/soft/resin/bin/resin.sh -server myhistory kill;sh /opt/soft/resin/bin/resin.sh -server myhistory start"
