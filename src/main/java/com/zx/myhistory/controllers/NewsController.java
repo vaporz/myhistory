7c34825ff2334a81ec68b016264d27bdd74c428e
vaporz <zzxx513@gmail.com>
2013-04-01 11:41:20 +0800

    code formate

--- src/main/java/com/zx/myhistory/controllers/NewsController.java
+++ src/main/java/com/zx/myhistory/controllers/NewsController.java
@@ -1,3 +1,4 @@
+
 package com.zx.myhistory.controllers;
 
 import com.zx.myhistory.model.BadRequestException;
@@ -27,118 +28,117 @@ import java.util.Set;
 @Path("")
 public class NewsController {
 
-	private Log logger = LogFactory.getLog(NewsController.class);
-	@Autowired
-	private NewsService newsService;
+    private Log logger = LogFactory.getLog(NewsController.class);
+    @Autowired
+    private NewsService newsService;
 
-	private static SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    private static SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
-	/**
-	 * 显示事件提交页
-	 */
-	@Get("news/commit")
-	public String showCommitNews(Invocation inv, String msg) {
-		inv.addModel("msg", StringUtils.defaultString(msg));
-		return "showcommitnews";
-	}
+    /**
+     * 显示事件提交页
+     */
+    @Get("news/commit")
+    public String showCommitNews(Invocation inv, String msg) {
+        inv.addModel("msg", StringUtils.defaultString(msg));
+        return "showcommitnews";
+    }
 
-	/**
-	 * 提交事件
-	 */
-	@Post("news/commit")
-	public String commitNews(Invocation inv, @Param("url") String url, @Param("title") String title,
-			@Param("content") String content, @Param("keywords") String keywords, @Param("newTime") String newTimeStr)
-			throws ParseException, BadRequestException {
-		Set<String> keywordSet = getKeywordSet(keywords);
-		if (!CollectionUtils.isEmpty(keywordSet)) {
-			long newsTime = format.parse(newTimeStr).getTime();
-			newsService.commitNews(title, content, url, newsTime, keywordSet);
-		}
-		return showCommitNews(inv, "操作成功");
-	}
+    /**
+     * 提交事件
+     */
+    @Post("news/commit")
+    public String commitNews(Invocation inv, @Param("url") String url, @Param("title") String title, @Param("content") String content,
+        @Param("keywords") String keywords, @Param("newTime") String newTimeStr) throws ParseException, BadRequestException {
+        Set<String> keywordSet = getKeywordSet(keywords);
+        if (!CollectionUtils.isEmpty(keywordSet)) {
+            long newsTime = format.parse(newTimeStr).getTime();
+            newsService.commitNews(title, content, url, newsTime, keywordSet);
+        }
+        return showCommitNews(inv, "操作成功");
+    }
 
-	private Set<String> getKeywordSet(String keywords) {
-		Set<String> keywordSet = new HashSet<String>();
-		if (StringUtils.isBlank(keywords)) {
-			return keywordSet;
-		}
-		String[] keywordsArr = keywords.split(",");
-		for (String keyword : keywordsArr) {
-			if (StringUtils.isBlank(keyword)) {
-				continue;
-			}
-			keyword = StringUtils.trimToEmpty(keyword);
-			keywordSet.add(keyword);
-		}
-		return keywordSet;
-	}
+    private Set<String> getKeywordSet(String keywords) {
+        Set<String> keywordSet = new HashSet<String>();
+        if (StringUtils.isBlank(keywords)) {
+            return keywordSet;
+        }
+        String[] keywordsArr = keywords.split(",");
+        for (String keyword : keywordsArr) {
+            if (StringUtils.isBlank(keyword)) {
+                continue;
+            }
+            keyword = StringUtils.trimToEmpty(keyword);
+            keywordSet.add(keyword);
+        }
+        return keywordSet;
+    }
 
-	/**
-	 * 显示关键词列表
-	 */
-	@Get("keywords")
-	public String listKerwords(Invocation inv) {
-		List<Keyword> list = newsService.getKeywords();
-		inv.addModel("keywords", list);
-		return "keywords";
-	}
+    /**
+     * 显示关键词列表
+     */
+    @Get("keywords")
+    public String listKerwords(Invocation inv) {
+        List<Keyword> list = newsService.getKeywords();
+        inv.addModel("keywords", list);
+        return "keywords";
+    }
 
-	/**
-	 * 按时间列出与一个关键词有关的事件
-	 */
-	@Get("keyword/{keywordId:[0-9]+}/news")
-	public String listNewsByKeyword(Invocation inv, @Param("keywordId") long keywordId,
-			@Param("newsTime") long newsTime, @Param("limit") int limit) throws BadRequestException {
-		if (limit <= 0 || newsTime < 0) {
-			throw new BadRequestException(ErrorCode.ErrorParameters, "wrong parameters");
-		}
-		Keyword keyword = newsService.getKeywordById(keywordId);
-		List<News> list = newsService.getNewsByKeyword(keywordId, newsTime, limit);
-		inv.addModel("keyword", keyword);
-		inv.addModel("news", list);
-		return "news";
-	}
+    /**
+     * 按时间列出与一个关键词有关的事件
+     */
+    @Get("keyword/{keywordId:[0-9]+}/news")
+    public String listNewsByKeyword(Invocation inv, @Param("keywordId") long keywordId, @Param("newsTime") long newsTime,
+        @Param("limit") int limit) throws BadRequestException {
+        if (limit <= 0 || newsTime < 0) {
+            throw new BadRequestException(ErrorCode.ErrorParameters, "wrong parameters");
+        }
+        Keyword keyword = newsService.getKeywordById(keywordId);
+        List<News> list = newsService.getNewsByKeyword(keywordId, newsTime, limit);
+        inv.addModel("keyword", keyword);
+        inv.addModel("news", list);
+        return "news";
+    }
 
-	/**
-	 * 显示一个事件
-	 */
-	@Get("news/{newsId:[0-9]+}")
-	public String showNews(Invocation inv, @Param("newsId") long newsId) {
-		News news = newsService.getOneNewsById(newsId);
-		List<Keyword> keywords = newsService.getKeywordsByNewsId(newsId);
-		inv.addModel("keywords", keywords);
-		inv.addModel("news", news);
-		return "onenews";
-	}
+    /**
+     * 显示一个事件
+     */
+    @Get("news/{newsId:[0-9]+}")
+    public String showNews(Invocation inv, @Param("newsId") long newsId) {
+        News news = newsService.getOneNewsById(newsId);
+        List<Keyword> keywords = newsService.getKeywordsByNewsId(newsId);
+        inv.addModel("keywords", keywords);
+        inv.addModel("news", news);
+        return "onenews";
+    }
 
-	/**
-	 * 给某个事件添加关键字
-	 */
-	@Post("news/{newsId:[0-9]+}/keywords")
-	public String moreKeywords(Invocation inv, @Param("newsId") long newsId, @Param("keywords") String keywords) {
-		Set<String> keywordSet = getKeywordSet(keywords);
-		if (!CollectionUtils.isEmpty(keywordSet)) {
-			newsService.attachKeywordsForNews(newsId, keywordSet);
-		}
-		return showNews(inv, newsId);
-	}
+    /**
+     * 给某个事件添加关键字
+     */
+    @Post("news/{newsId:[0-9]+}/keywords")
+    public String moreKeywords(Invocation inv, @Param("newsId") long newsId, @Param("keywords") String keywords) {
+        Set<String> keywordSet = getKeywordSet(keywords);
+        if (!CollectionUtils.isEmpty(keywordSet)) {
+            newsService.attachKeywordsForNews(newsId, keywordSet);
+        }
+        return showNews(inv, newsId);
+    }
 
-	@Get("keyword/merge")
-	public String showMergeKeyword(Invocation inv, String msg) {
-		inv.addModel("msg", StringUtils.defaultString(msg));
-		return "mergekeyword";
-	}
+    @Get("keyword/merge")
+    public String showMergeKeyword(Invocation inv, String msg) {
+        inv.addModel("msg", StringUtils.defaultString(msg));
+        return "mergekeyword";
+    }
 
-	/**
-	 * 合并关键字
-	 */
-	@Post("keyword/merge")
-	public String mergeKeywords(Invocation inv, @Param("keyword") String keyword, @Param("target") String target)
-			throws BadRequestException {
-		if (StringUtils.isBlank(keyword) || StringUtils.isBlank(target)) {
-			throw new BadRequestException(ErrorCode.ErrorParameters, "wrong parameters");
-		}
-		newsService.mergeKerword(keyword, target);
-		return showMergeKeyword(inv, "操作成功");
-	}
+    /**
+     * 合并关键字
+     */
+    @Post("keyword/merge")
+    public String mergeKeywords(Invocation inv, @Param("keyword") String keyword, @Param("target") String target)
+                                                                                                                 throws BadRequestException {
+        if (StringUtils.isBlank(keyword) || StringUtils.isBlank(target)) {
+            throw new BadRequestException(ErrorCode.ErrorParameters, "wrong parameters");
+        }
+        newsService.mergeKerword(keyword, target);
+        return showMergeKeyword(inv, "操作成功");
+    }
 }
--- src/main/java/com/zx/myhistory/service/NewsService.java
+++ src/main/java/com/zx/myhistory/service/NewsService.java
@@ -1,3 +1,4 @@
+
 package com.zx.myhistory.service;
 
 import com.zx.myhistory.biz.NewsBiz;
@@ -16,101 +17,101 @@ import java.util.Set;
 
 @Service
 public class NewsService {
-	@Autowired
-	private NewsBiz newsBiz;
+    @Autowired
+    private NewsBiz newsBiz;
 
-	public void commitNews(String title, String content, String url, long newsTime, Set<String> keywordStrSet) {
-		long newsId = newsBiz.commitNews(title, content, url, newsTime);
-		attachKeywordsForNews(newsId, newsTime, keywordStrSet);
-	}
+    public void commitNews(String title, String content, String url, long newsTime, Set<String> keywordStrSet) {
+        long newsId = newsBiz.commitNews(title, content, url, newsTime);
+        attachKeywordsForNews(newsId, newsTime, keywordStrSet);
+    }
 
-	public List<Keyword> getKeywords() {
-		return newsBiz.getKeywords();
-	}
+    public List<Keyword> getKeywords() {
+        return newsBiz.getKeywords();
+    }
 
-	public List<News> getNewsByKeyword(long keywordId, long newsTime, int limit) {
-		newsTime = newsTime <= 0 ? Long.MAX_VALUE : newsTime;
-		return newsBiz.getNewsByKeyword(keywordId, newsTime, limit);
-	}
+    public List<News> getNewsByKeyword(long keywordId, long newsTime, int limit) {
+        newsTime = newsTime <= 0 ? Long.MAX_VALUE : newsTime;
+        return newsBiz.getNewsByKeyword(keywordId, newsTime, limit);
+    }
 
-	public News getOneNewsById(long newsId) {
-		return newsBiz.getOneNewsById(newsId);
-	}
+    public News getOneNewsById(long newsId) {
+        return newsBiz.getOneNewsById(newsId);
+    }
 
-	public Keyword getKeywordById(long keywordId) {
-		Keyword keyword = newsBiz.getKeywordById(keywordId);
-		while (keyword.getAliasId() > 0) {
-			keyword = newsBiz.getKeywordById(keyword.getAliasId());
-		}
-		return keyword;
-	}
+    public Keyword getKeywordById(long keywordId) {
+        Keyword keyword = newsBiz.getKeywordById(keywordId);
+        while (keyword.getAliasId() > 0) {
+            keyword = newsBiz.getKeywordById(keyword.getAliasId());
+        }
+        return keyword;
+    }
 
-	public List<Keyword> getKeywordsByNewsId(long newsId) {
-		return newsBiz.getKeywordsByNewsId(newsId);
-	}
+    public List<Keyword> getKeywordsByNewsId(long newsId) {
+        return newsBiz.getKeywordsByNewsId(newsId);
+    }
 
-	public void attachKeywordsForNews(long newsId, Set<String> keywordStrSet) {
-		News news = newsBiz.getOneNewsById(newsId);
-		attachKeywordsForNews(newsId, news.getNewsTime(), keywordStrSet);
-	}
+    public void attachKeywordsForNews(long newsId, Set<String> keywordStrSet) {
+        News news = newsBiz.getOneNewsById(newsId);
+        attachKeywordsForNews(newsId, news.getNewsTime(), keywordStrSet);
+    }
 
-	private void attachKeywordsForNews(long newsId, long newsTime, Set<String> keywordStrSet) {
-		Set<Keyword> keywordSet = getKeywords(keywordStrSet, true);
-		// TODO 如果有存在alias的关键字，要找到根源的关键字
-		// TODO 有并发的问题，如果有关键字在执行过程中被alias？
-		newsBiz.commitKeywordNews(newsId, newsTime, keywordSet);
-		newsBiz.commitNewsKeyword(newsId, keywordSet);
-	}
+    private void attachKeywordsForNews(long newsId, long newsTime, Set<String> keywordStrSet) {
+        Set<Keyword> keywordSet = getKeywords(keywordStrSet, true);
+        // TODO 如果有存在alias的关键字，要找到根源的关键字
+        // TODO 有并发的问题，如果有关键字在执行过程中被alias？
+        newsBiz.commitKeywordNews(newsId, newsTime, keywordSet);
+        newsBiz.commitNewsKeyword(newsId, keywordSet);
+    }
 
-	private Set<Keyword> getKeywords(Set<String> keywordStrSet, boolean createWhenUnxist) {
-		Set<String> keywordLowercase = new HashSet<String>();
-		for (String keyword : keywordStrSet) {
-			keywordLowercase.add(keyword.toLowerCase());
-		}
-		Map<String, Keyword> keywords = newsBiz.findKeywordsReturnMap(keywordLowercase);
-		if (createWhenUnxist) {
-			for (String keyword : keywordStrSet) {
-				if (keywords.get(keyword) == null) {
-					// 新keyword，添加进去
-					long keywordId = newsBiz.createKeyword(keyword);
-					keywords.put(keyword, new Keyword(keywordId, keyword));
-				}
-			}
-		}
-		Set<Keyword> keywordSet = new HashSet<Keyword>();
-		CollectionUtils.addAll(keywordSet, keywords.values().iterator());
-		return keywordSet;
-	}
+    private Set<Keyword> getKeywords(Set<String> keywordStrSet, boolean createWhenUnxist) {
+        Set<String> keywordLowercase = new HashSet<String>();
+        for (String keyword : keywordStrSet) {
+            keywordLowercase.add(keyword.toLowerCase());
+        }
+        Map<String, Keyword> keywords = newsBiz.findKeywordsReturnMap(keywordLowercase);
+        if (createWhenUnxist) {
+            for (String keyword : keywordStrSet) {
+                if (keywords.get(keyword) == null) {
+                    // 新keyword，添加进去
+                    long keywordId = newsBiz.createKeyword(keyword);
+                    keywords.put(keyword, new Keyword(keywordId, keyword));
+                }
+            }
+        }
+        Set<Keyword> keywordSet = new HashSet<Keyword>();
+        CollectionUtils.addAll(keywordSet, keywords.values().iterator());
+        return keywordSet;
+    }
 
-	public void mergeKerword(String keyword, String target) {
-		Keyword keywordObj = newsBiz.getKeywordByName(keyword.toLowerCase());
-		Keyword targetObj = newsBiz.getKeywordByName(target.toLowerCase());
-		if (keywordObj == null || targetObj == null) {
-			return;
-		}
-		List<News> news = newsBiz.getNewsIdByKeyword(keywordObj.getKeywordId(), Long.MAX_VALUE, Integer.MAX_VALUE);
-		List<News> targetNews = newsBiz.getNewsIdByKeyword(targetObj.getKeywordId(), Long.MAX_VALUE, Integer.MAX_VALUE);
-		Set<Long> targetNewsId = new HashSet<Long>();
-		for (News item : targetNews) {
-			targetNewsId.add(item.getNewsId());
-		}
-		List<News> newsTobeDeleted = new ArrayList<News>();
-		List<News> newsTobeUpdated = new ArrayList<News>();// 有可能会并发又产生一个news不在这个列表中
-		for (News item : news) {
-			if (targetNewsId.contains(item.getNewsId())) {
-				newsTobeDeleted.add(item);
-			} else {
-				newsTobeUpdated.add(item);
-			}
-		}
-		for (News item : newsTobeDeleted) {
-			newsBiz.deleteNewsFromNewsKeyword(item.getNewsId(), keywordObj.getKeywordId());
-		}
-		for (News item : newsTobeUpdated) {
-			newsBiz.updateKeywordIdForNewsKeyword(item.getNewsId(), keywordObj.getKeywordId(), targetObj.getKeywordId());
-		}
-		newsBiz.deleteNewsByKeywordId(keywordObj.getKeywordId());
-		newsBiz.aliasKeyword(keywordObj.getKeywordId(), targetObj.getKeywordId());
-		newsBiz.redirectAlias(keywordObj.getKeywordId(), targetObj.getKeywordId());
-	}
+    public void mergeKerword(String keyword, String target) {
+        Keyword keywordObj = newsBiz.getKeywordByName(keyword.toLowerCase());
+        Keyword targetObj = newsBiz.getKeywordByName(target.toLowerCase());
+        if (keywordObj == null || targetObj == null) {
+            return;
+        }
+        List<News> news = newsBiz.getNewsIdByKeyword(keywordObj.getKeywordId(), Long.MAX_VALUE, Integer.MAX_VALUE);
+        List<News> targetNews = newsBiz.getNewsIdByKeyword(targetObj.getKeywordId(), Long.MAX_VALUE, Integer.MAX_VALUE);
+        Set<Long> targetNewsId = new HashSet<Long>();
+        for (News item : targetNews) {
+            targetNewsId.add(item.getNewsId());
+        }
+        List<News> newsTobeDeleted = new ArrayList<News>();
+        List<News> newsTobeUpdated = new ArrayList<News>();// 有可能会并发又产生一个news不在这个列表中
+        for (News item : news) {
+            if (targetNewsId.contains(item.getNewsId())) {
+                newsTobeDeleted.add(item);
+            } else {
+                newsTobeUpdated.add(item);
+            }
+        }
+        for (News item : newsTobeDeleted) {
+            newsBiz.deleteNewsFromNewsKeyword(item.getNewsId(), keywordObj.getKeywordId());
+        }
+        for (News item : newsTobeUpdated) {
+            newsBiz.updateKeywordIdForNewsKeyword(item.getNewsId(), keywordObj.getKeywordId(), targetObj.getKeywordId());
+        }
+        newsBiz.deleteNewsByKeywordId(keywordObj.getKeywordId());
+        newsBiz.aliasKeyword(keywordObj.getKeywordId(), targetObj.getKeywordId());
+        newsBiz.redirectAlias(keywordObj.getKeywordId(), targetObj.getKeywordId());
+    }
 }
